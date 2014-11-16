0c927ba0305c0290e74de70b939807f728ecb580
CassouletMan <k.delplanque@gmail.com>
2014-11-16 11:30:39 +0100

    Test git commit and push for windows pc

--- src/Engine.js
+++ src/Engine.js
@@ -6,8 +6,9 @@ var Engine = function () {
 
     var game_board = new Array(6);
     var current_player;
-    var tabJoueur1 = new Array(36);
-    var tabJoueur2 = new Array(36);
+    var tabJoueur1 = new Array();
+    var tabJoueur2 = new Array();
+    var nbJetonJouable = new Array();
 
 
 
@@ -72,6 +73,14 @@ var Engine = function () {
     };
 
 
+    this.change_player = function () {
+        if (current_player == "Joueur1")
+            current_player = "Joueur2";
+        else
+            current_player = "Joueur1";
+    };
+
+
     this.getColorAt = function(i,j) {
 
         var I;
@@ -110,6 +119,16 @@ var Engine = function () {
         game_board[I][j-1]=null;
     };
 
+    this.takeAtWithCoordNumber = function(i,j) {
+
+        if(current_player=="Joueur1")
+            tabJoueur1.push(game_board[i][j]);
+        else
+            tabJoueur2.push(game_board[i][j]);
+
+        game_board[i][j]=null;
+    };
+
 
 
 
@@ -123,20 +142,20 @@ var Engine = function () {
         {
             for(j = 0; j < 6; j++)
             {
-                if(j+1<6)
+                if(j+1<=5)
                     if(game_board[i][j] == game_board[i][j+1])
                         return false;
 
-                if(j-1>0)
+                if(j-1>=0)
                     if(game_board[i][j] == game_board[i][j-1])
                         return false;
 
 
-                if(i+1<6)
+                if(i+1<=5)
                     if(game_board[i][j] == game_board[i+1][j])
                         return false;
 
-                if(i-1>0)
+                if(i-1>=0)
                     if(game_board[i][j] == game_board[i-1][j])
                         return false;
 
@@ -159,19 +178,114 @@ var Engine = function () {
         }
 
         return res;
-    }
+    };
 
     this.countColorOfCurrentPlayer = function(color)
     {
         var res=0;
+        if(current_player=="Joueur1") {
+            for (var i = 0; i < tabJoueur1.length; i++) {
 
-        for(var i =0 ; i< tabJoueur1.length;i++)
+
+                if (tabJoueur1[i] == color)
+                    res++;
+
+            }
+        }
+        else
         {
-            if(tabJoueur1[i]==color)
-                res++;
+            for (var i = 0; i < tabJoueur2.length; i++) {
+
+
+                if (tabJoueur2[i] == color)
+                    res++;
+
+            }
         }
 
         return res;
-    }
+    };
+
+    this.ColorOfNbJetonJouable = function(color)
+    {
+
+        for(var i =0 ; i< nbJetonJouable.length;i++)
+        {
+            if(nbJetonJouable[i]==color)
+                return true;
+        }
+
+        return false;
+    };
+
+    this.chooseColor = function(color)
+    {
+        var i;
+        var j;
+        var rst=0;
+
+        for(i = 0; i < nbJetonJouable; i=i+3) {
+
+            console.log(nbJetonJouable[i]);
+
+            if(color === nbJetonJouable[i]) {
+                this.takeAtWithCoordNumber(nbJetonJouable[i + 1], nbJetonJouable[i + 2]);
+
+                console.log(nbJetonJouable[i + 1]);
+                console.log(nbJetonJouable[i + 2]);
+
+            }
+        }
+    };
+
+
+    this.listOfPossibilities = function() {
+
+        var i;
+        var j;
+        var rst=0;
+
+        for(i = 0; i < 6; i++)
+        {
+            for(j = 0; j < 6; j++) {
+
+                if (game_board[i][j] != null) {
+
+                    rst = 0;
+
+                    if (j + 1 <= 5)
+                        if (game_board[i][j + 1] != null)
+                            rst++;
+                    if (j - 1 >= 0)
+                        if (game_board[i][j - 1] != null)
+                            rst++;
+                    if (i + 1 <= 5)
+                        if (game_board[i + 1][j] != null)
+                            rst++;
+                    if (i - 1 >= 0)
+                        if (game_board[i - 1][j] != null)
+                            rst++;
+
+                    //console.log(rst);
+
+                    if (rst < 3) {
+                        nbJetonJouable.push(game_board[i][j]);
+                        nbJetonJouable.push(i);
+                        nbJetonJouable.push(j);
+                    }
+                }
+                //else
+                    //console.log("VIDE");
+
+
+            }
+
+            //console.log("\n");
+        }
+
+        //console.log(nbJetonJouable.length/3);
+
+        return (nbJetonJouable.length/3);
+    };
 
 };
--- test/test.js
+++ test/test.js
@@ -30,3 +30,23 @@ PalettoTestCase.prototype.testStory3 = function () {
     assertTrue(g.count()==35);
     assertTrue(g.countColorOfCurrentPlayer("jaune")==1);
 };
+
+PalettoTestCase.prototype.testStory4 = function () {
+
+    var g = new Engine();
+    g.init();
+    g.takeAt('A',6);
+    g.change_player();
+    assertTrue(g.getCurrentPlayer()=="Joueur2");
+    assertTrue(g.listOfPossibilities()==5);
+    assertTrue(g.ColorOfNbJetonJouable("blanc"));
+    assertTrue(g.ColorOfNbJetonJouable("bleu"));
+    assertTrue(g.ColorOfNbJetonJouable("noir"));
+    assertTrue(g.ColorOfNbJetonJouable("rouge")==false);
+    assertTrue(g.ColorOfNbJetonJouable("vert")==false);
+    assertTrue(g.ColorOfNbJetonJouable("jaune")==false);
+    g.chooseColor("noir");
+    //console.log(g.countColorOfCurrentPlayer());
+    assertTrue(g.countColorOfCurrentPlayer("noir")==2);
+};
+
